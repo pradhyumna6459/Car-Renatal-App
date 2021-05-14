0df1455348ce8bf1f73639cd43ca86888272c083
Pradhyumna Singh rathore <pradhyumnarathore@gmail.com>
2021-05-14 09:12:34 +0530

    added

--- config/dev.js
+++ config/dev.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 module.exports=
     {
-        MongoDB:"mongodb://localhost:27017/carRental",
-        AWSAccessID:"AKIA6IDGMDIZDFS6CLOE",
-        AWSSecretKey:'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',
-        StripePublishableKey:'pk_test_51IqgCTSE6pfpg6r3Rbf5suioRCCMDdixSXMOK7X57a3cXnk8BKiuZbVJW3gqkAToww3Jx3Oy8n2FR2riexUJOrmo00ItWCpH98',
-        StripeSecreatKey:'sk_test_51IqgCTSE6pfpg6r3RSw85BkUeDltpW3ae4BccaN264Nv3uFY6FqFzpLbQABJ1omTCa3LydcpJLvsaAec175re6jS00Uizgghr0'
+        MongoDB:process.env.MONGODB,
+        AWSAccessID:process.env.AWSACCESSID,
+        AWSSecretKey:process.env.AWSSECRETKET,
+        StripePublishableKey:process.env.STRIPEPUBLISHABLEKEY,
+        StripeSecreatKey:process.env.STRIPESECREATKEY
     }
